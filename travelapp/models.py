94f2d7d2f70e6e84a8eb53466ce0c486622abb96
jun <jun020216@naver.com>
2022-11-05 22:02:33 +0900

    media settings

--- travelapp/models.py
+++ travelapp/models.py
@@ -1,31 +1,44 @@
 from django.db import models
 
+TRAVEL_TYPE = (
+    ('A', 'ROCK'),
+    ('B', 'ORANGE'),
+    ('C', 'SUNSET'),
+    ('D', 'PIG'),
+    ('E', 'FLOWER')
+)
+
+PLACE_TYPE = (
+    ('RESTAURANT', 'RESTAURANT'),
+    ('CAFE', 'CAFE'),
+)
+
 # Create your models here.
 class Region(models.Model):
-    name = models.CharField(max_length=20)
-    region_large = models.CharField(max_length=20)
-    latitude = models.IntegerField()
-    longtitude = models.IntegerField()
+    region_small = models.CharField(max_length=20, null=False)
+    region_large = models.CharField(max_length=20, null=False)
+    latitude = models.IntegerField(null=False)
+    longtitude = models.IntegerField(null=False)
 
     def __str__(self):
         return self.name
         
-class PlaceType(models.Model):
-    name = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.name
 
 
 class Place(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.TextField()
-    latitude = models.IntegerField()
-    longtitude = models.IntegerField()
-    place_type = models.ForeignKey(PlaceType, verbose_name="place_type", on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, null=False)
+    description = models.TextField(null=True, default="")
+    latitude = models.IntegerField(null=False)
+    longtitude = models.IntegerField(null=False)
     region = models.ForeignKey(Region, verbose_name="region", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
+class PlaceImage(models.Model):
+    place = models.ForeignKey(Place, verbose_name="place", on_delete=models.CASCADE)
+    src = models.URLField(null=True, blank=True)
+    index = models.IntegerField(null=True, default=1)
     
+    def __str__(self):
+        return self.src
