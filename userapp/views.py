13dc8bfd5e1d72f08869d6e53bb06df656a04ab3
kweonminsung <kevin6528@scg.skku.ac.kr>
2022-11-06 03:16:34 +0900

    create user model & POST /users/me 구현

--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+import json
+
+from userapp.models import User
 
 # Create your views here.
+class UserPersonalityCreateView(View):
+    def post(self, request):
+        if(request.user.is_anonymous):
+            return JsonResponse({"message": "Unauthorized"})
+        data = json.loads(self.request.body)
+        user = get_object_or_404(User, id=request.user.id)
+        user.person_type = data["person_type"]
+        user.select_view = data["preference"]["view"]
+        user.select_cafe = data["preference"]["cafe"]
+        user.select_drink = data["preference"]["drink"]
+        user.select_activity = data["preference"]["activity"]
+        user.select_food = data["preference"]["food"]
+        user.save()
+        return JsonResponse({"message": "success"})
