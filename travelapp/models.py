51aeb6eeae4c50727d0da51c2019a3cc9f7ed7e0
jun <jun020216@naver.com>
2022-11-05 22:49:08 +0900

    model 설계

--- travelapp/models.py
+++ travelapp/models.py
@@ -15,14 +15,11 @@ PLACE_TYPE = (
 
 # Create your models here.
 class Region(models.Model):
-    region_small = models.CharField(max_length=20, null=False)
-    region_large = models.CharField(max_length=20, null=False)
-    latitude = models.IntegerField(null=False)
-    longtitude = models.IntegerField(null=False)
+    region_small = models.CharField(max_length=20, null=False)   # address [1]
+    region_large = models.CharField(max_length=20, null=False)   # address [0]
 
     def __str__(self):
         return self.name
-        
 
 
 class Place(models.Model):
@@ -42,3 +39,14 @@ class PlaceImage(models.Model):
     
     def __str__(self):
         return self.src
+
+class PlaceTag(models.Model):
+    name = models.TextField()
+    def __str__(self):
+        return self.name
+
+class TagToPlace(models.Model):
+    place_id = models.ForeignKey(Place, verbose_name="place_id", on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(PlaceTag, verbose_name="tag_id", on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
